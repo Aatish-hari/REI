3cc46e525e69529a5072d48ea2ff9f0d3b86f857
Aatish-hari <aatishhari2006@gmail.com>
2026-03-21 02:36:56 +0000

    echo improvement

--- commands.c
+++ commands.c
@@ -5,9 +5,9 @@ void CommandExecution(char** tokens, char** env){
 //echo command====>
     if(strcmp(tokens[0], "echo") == 0){
         for(int i = 1; tokens[i] ; i++){
-            printf(tokens[i]);
-            if(tokens[i+1])
-            printf("\n");
+           printf("%s ", tokens[i]);
+            if(!tokens[i+1])
+                printf("\n");
         }
     }
 
